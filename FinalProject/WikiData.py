a9cfb3e3ab10fb3e01719da8ad5a161d91f200ba
Aidan White <aidan.white36@gmail.com>
2017-11-29 14:25:52 +0100

    Add test script to take data from wiki and analyze

--- FinalProject/WikiData.py
+++ FinalProject/WikiData.py
@@ -1,11 +1,35 @@
 import requests as rq
 import sys
+import string
 
-def get_wiki(team):
+badWords = list(string.punctuation)
+numbers = ["0","1","2","3","4","5","6","7","8","9"]
+badWords = badWords + numbers;
+badWords.remove(".")
+badWords.remove(",")
+badWords.remove("?")
+badWords.remove("!")
+
+def get_wiki_markup(team):
     url = "https://en.wikipedia.org/w/api.php?action=query&titles={0}&prop=revisions&rvprop=content&format=json".format(
         team)
     d = rq.get(url).text
     return d
+
+def get_cleaned_text(team):
+    text = get_wiki_markup(team)
+    words = text.split(" ")
+    cleanedText = []
+    for word in words:
+        check = True
+        for badWord in badWords:
+            if badWord in word or word == "" or len(word) < 5:
+                check = False
+                break
+        if (check == True):
+            cleanedText.append(word)
+    text = " ".join(cleanedText)
+    return text
 
 if __name__ == "__main__":
     try:
@@ -13,4 +37,5 @@ if __name__ == "__main__":
     except:
         team = "Minnesota_Timberwolves"
     print team
-    print get_wiki(team)
+    get_cleaned_text(team)
+    #print get_wiki(team)
