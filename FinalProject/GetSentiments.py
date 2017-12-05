1457cc18534a9266ec09a3f203b11aae57c25cde
Aidan White <aidan.white36@gmail.com>
2017-12-05 22:15:08 +0100

    Trying to get history sentients with groups of sentences

--- FinalProject/GetSentiments.py
+++ FinalProject/GetSentiments.py
@@ -8,19 +8,38 @@ team_data = {}
 out = []
 
 for name in os.listdir('./history/'):
+    sad = 0
+    joy = 0
     with open('./history/' + name, 'r') as f:
         hist = f.read()
-    tones = toneAnalyzer(hist, name)
-    team_data[name] = tones
+    sentences = hist.split('.')
+    for i in range(len(sentences)/100):
+        group = ""
+        for j in range(100):
+            group += sentences[j + i * 100]
+        tones = toneAnalyzer(group, name)
+        print tones.keys()
+        # if 'Sadness' in tones.keys():
+        #     sad += 1
+        # if 'Joy' in tones.keys():
+        #     joy += 1
+        # team_data = {}
+        # team_data['team'] = name
+        # team_data['joy'] = joy
+        # team_data['sadness'] = sad
+        # out.append(team_data)
+        # i += 1
+        # print i
     print 'finished sentiment: ' + name
 
-for team in team_data.keys():
-    team_tones = {}
-    team_tones['team'] = team
-    for tone in team_data[team]:
-        team_tones[tone['tone_name']] = str(tone['score'])
-    out.append(team_tones)
-    print 'json added: ' + team
+
+# for team in team_data.keys():
+#     team_tones = {}
+#     team_tones['team'] = team
+#     for tone in team_data[team]:
+#         team_tones[tone['tone_name']] = str(tone['score'])
+#     out.append(team_tones)
+#     print 'json added: ' + team
 
 with open('./sentiment_data/history_sentiments.json', 'w') as f:
     json.dump(out, f)
