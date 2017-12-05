1a601cf6f26fc2231254175f3f2474ce4a7d54e1
Aidan White <aidan.white36@gmail.com>
2017-12-05 18:52:45 +0100

    Plot for joy added to images

--- FinalProject/JoyPlot.py
+++ FinalProject/JoyPlot.py
@@ -8,23 +8,31 @@ def load_sentiment_scores():
     sentiment_data = json.loads(string)
     return sentiment_data
 
+def filename_to_name(filename):
+    filename = filename.replace('.txt', '')
+    filename = filename.replace('_', ' ')
+    l = filename.split(' ')
+    return l[-1]
+
+
 def main():
     data = load_sentiment_scores()
     teams = []
     scores = []
     for team in data:
-        teams.append(team['team'])
+        teams.append(filename_to_name(team['team']))
         try:
             scores.append(float(team['Joy']))
         except:
             scores.append(0.0)
-    # import matplotlib.pyplot as plt
-    # listofnames = ['Al', 'Ca', 'Re', 'Ma', 'Al', 'Ma', 'Ma', 'Re', 'Ca']
     xs = [i + 0.1 for i, _ in enumerate(teams)]
     plt.bar(xs, scores, .8)
     plt.xticks([i + .1 for i, _ in enumerate(teams)], teams, rotation='vertical')
     axes = plt.gca()
     axes.set_ylim([.45,.7])
+    plt.title("Joy Scores")
+    plt.xlabel('Team')
+    plt.ylabel('Score')
     plt.show()
 
 
