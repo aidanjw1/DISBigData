af59987805456358941202fb1286de05ecd067d0
Chris Tordi <christordi@MacBook-Pro-9.local>
2017-12-06 02:28:00 +0100

    Added data

--- FinalProject/GetTweetSentiments.py
+++ FinalProject/GetTweetSentiments.py
@@ -7,12 +7,12 @@ tweet_data = []
 def getSentiment():
     count = 0
     #for name in os.listdir('./Tweets/'):
-    with open('./Tweets/@nyknicks.txt', 'r') as f:
+    with open('./Tweets/@okcthunder.txt', 'r') as f:
         content = f.readlines();
         for line in content:
             print(line)
             try:
-                tweet_info = toneAnalyzer(line, "@nyknicks")
+                tweet_info = toneAnalyzer(line, "@okcthunder")
             except:
                 pass
 
