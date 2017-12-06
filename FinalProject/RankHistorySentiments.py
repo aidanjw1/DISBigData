5ce5e0dffb833d35fd109c6bcd8bb784212de02e
Aidan White <aidan.white36@gmail.com>
2017-12-06 17:09:09 +0100

    Ranking history sentiments: joy, sad, difference

--- FinalProject/RankHistorySentiments.py
+++ FinalProject/RankHistorySentiments.py
@@ -13,6 +13,12 @@ def rank_scores(sentiment):
     rankings.sort(reverse = True)
     return rankings
 
+def rank_score_diffs(sent1, sent2):
+    scores = load_sentiment_scores()
+    rankings = [(round(data[sent1] - data[sent2], 3), data['team']) for data in scores]
+    rankings.sort(reverse = True)
+    return rankings
+
 def write_to_csv(data, filename):
     with open('./sentiment_data/' + filename, 'w') as f:
         fieldnames = ['Team', 'Score']
@@ -25,8 +31,10 @@ def write_to_csv(data, filename):
 def main():
     joys = rank_scores('joy')
     sads = rank_scores('sadness')
+    diffs = rank_score_diffs('joy', 'sadness')
 
     write_to_csv(joys, 'joy_rankings.csv')
     write_to_csv(sads, 'sadness_rankings.csv')
+    write_to_csv(diffs, 'difference_rankings.csv')
 
 main()
